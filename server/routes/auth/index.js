e504b441f516ac5fc7177c68ceb01320bc3c0081
kevin tauzer <kevint@kevins-MBP.home>
2019-02-26 17:19:07 -1000

    create register route

---
+++ server/routes/auth/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const User = require('../../../database/models/User');
+
+const saltRounds = 12;
+
+router.post('/register'), (req,res) => {
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if(err) {
+      res.status(500).json(err);
+    }
+    bcrypt.hash(req.body.password, salt, function(err, hash){
+      if(err) {
+        res.status(500).json(err);
+      }
+      return new User({
+        username: req.body.username,
+        password: hash
+      })
+      .save()
+      .then(() => {
+        return res.json({ success: true });
+      })
+      .catch((err) => {
+        return res.status(500).json(err)
+      })
+    })
+  })
+}
