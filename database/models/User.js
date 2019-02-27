d4295ada0a97d8aba2fae433d902f6b0da1958f7
Brad McKinney <brad.mckin@gmail.com>
2019-02-26 15:25:26 -1000

    create a Role model with a relation to users

--- database/models/User.js
+++ database/models/User.js
@@ -1,12 +1,14 @@
 const bookshelf = require('./bookshelf');
 const Item = require('./Item');
-const UserStatus = require('./UserStatus.js');
+const UserStatus = require('./UserStatus');
+const Role = require('./Role');
 
 class User extends bookshelf.Model {
   get tableName() { return 'users'; }
   get hasTimestamps() { return true; }
   items() { return this.hasMany('Item', 'items'); }
   status() { return this.hasOne('UserStatus', 'id', 'status_id'); }
+  role() { return this.hasOne('Role', 'id', 'role_id'); }
 }
 
 module.exports = bookshelf.model('User', User);
