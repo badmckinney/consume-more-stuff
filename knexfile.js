221bd79599e8627e694a7ebc37909297bd952967
Brad McKinney <brad.mckin@gmail.com>
2019-02-26 15:16:24 -1000

    match host variable in knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: {
-    host: process.env.PG_HOST,
+    host: process.env.PG_HOST_PORT,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE
