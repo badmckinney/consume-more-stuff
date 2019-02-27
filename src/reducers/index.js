176e6a5bb782165f7e0c1025b256e7b1be827e1e
Gene83 <gene83@protonmail.com>
2019-02-26 21:22:21 -1000

    install react-dom, react-scripts, react-router-dom, react-redux, redux-thunk and node-sass, write out boiler-plate react setup with redux and proof of concept for react router

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+// import actions here
+
+const initialState = {
+  items: []
+};
+
+const itemReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default itemReducer;
