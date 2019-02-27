7fbcbd23731e3a95d504c18c78bf9d05922b4c90
kevin tauzer <kevint@kevins-MBP.home>
2019-02-26 17:21:28 -1000

    create login route

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -6,26 +6,31 @@ const User = require('../../../database/models/User');
 
 const saltRounds = 12;
 
-router.post('/register'), (req,res) => {
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if(err) {
-      res.status(500).json(err);
-    }
-    bcrypt.hash(req.body.password, salt, function(err, hash){
-      if(err) {
+router.post('/register'),
+  (req, res) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
         res.status(500).json(err);
       }
-      return new User({
-        username: req.body.username,
-        password: hash
-      })
-      .save()
-      .then(() => {
-        return res.json({ success: true });
-      })
-      .catch((err) => {
-        return res.status(500).json(err)
-      })
-    })
-  })
-}
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+        if (err) {
+          res.status(500).json(err);
+        }
+        return new User({
+          username: req.body.username,
+          password: hash
+        })
+          .save()
+          .then(() => {
+            return res.json({ success: true });
+          })
+          .catch(err => {
+            return res.status(500).json(err);
+          });
+      });
+    });
+  };
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.json({ success: true, username: req.user.username, id: req.user.id });
+});
