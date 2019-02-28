fcfa75cdd34d92bdb7b03a0cbfa28b6a9e90cb2a
kevin tauzer <kevint@kevins-MBP.home>
2019-02-27 16:30:10 -1000

    create action to add post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 export const LOGOUT = 'LOGOUT';
+export const ADD_POST = 'ADDPOST';
 
 export const logout = () => {
   return dispatch => {
@@ -15,6 +16,29 @@ export const logout = () => {
 
         // add Error handling here for
         // unsuccessful logout
+      });
+  };
+};
+
+export const addPost = newPost => {
+  return dispatch => {
+    return fetch('api/items/new', {
+      method: 'POST',
+      body: JSON.stringify(newPost),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        return dispatch({
+          type: ADD_POST,
+          payload: newPost
+        }).catch(err => {
+          res.status(500).json(err);
+        });
       });
   };
 };
