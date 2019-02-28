b7ee4008adc3cddc123a188a03251ea208e5d837
kevin tauzer <kevint@kevins-MBP.home>
2019-02-28 12:11:57 -1000

    addding new item as a route

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+// import { connect } from 'react-redux';
 import './App.scss';
 
 import Sidebar from '../../components/Sidebar';
@@ -8,6 +8,7 @@ import Sidebar from '../../components/Sidebar';
 import Header from '../Header';
 import Login from '../Login';
 import Register from '../Register';
+import NewItem from '../NewItem'
 
 // import actions here
 
@@ -19,10 +20,10 @@ class App extends Component {
         <Sidebar />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/items/new" component={NewItem} />
         {/* <Route path="/" component={} />
         <Route path="/profile" component={} />
         <Route path="/messages" component={} />
-        <Route path="/items/new" component={} />
         <Route path="/items" component={} />
         <Route path="/category/:category" component={} /> */}
       </div>
