752a7a731e1eefca9b136c94c68841225b7bc5f6
Gene83 <gene83@protonmail.com>
2019-02-27 17:30:52 -1000

    update server routes for register and login, add passport.initialize() and passport.session() middleware

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -6,30 +6,34 @@ const User = require('../../../database/models/User');
 
 const saltRounds = 12;
 
-router.post('/register'),
-  (req, res) => {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
+router.post('/register', (req, res) => {
+  const newUser = req.body;
+
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) {
+      res.status(500).json(err);
+    }
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
       if (err) {
         res.status(500).json(err);
       }
-      bcrypt.hash(req.body.password, salt, function(err, hash) {
-        if (err) {
-          res.status(500).json(err);
-        }
-        return new User({
-          username: req.body.username,
-          password: hash
+      return new User({
+        first_name: newUser.first_name,
+        last_name: newUser.last_name,
+        email: newUser.email,
+        username: newUser.username,
+        password: hash
+      })
+        .save()
+        .then(() => {
+          return res.json({ success: true });
         })
-          .save()
-          .then(() => {
-            return res.json({ success: true });
-          })
-          .catch(err => {
-            return res.status(500).json(err);
-          });
-      });
+        .catch(err => {
+          return res.status(500).json(err);
+        });
     });
-  };
+  });
+});
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({ success: true, username: req.user.username, id: req.user.id });
--- server/server.js
+++ server/server.js
@@ -4,15 +4,27 @@ const bodyParser = require('body-parser');
 const redis = require('connect-redis')(session);
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
-const LocalStrategy = require('passport-local')
+const LocalStrategy = require('passport-local');
 const User = require('../database/models/User');
-const { auth } = require('./routes')
+const { auth } = require('./routes');
 
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 const SESSION_SECRET = process.env.SESSION_SECRET || 'squirtle';
 const REDIS_HOST_PORT = process.env.REDIS_HOST_PORT;
 const REDIS_URL = process.env.REDIS_URL;
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || 'development';
+
+if (!PORT) {
+  throw new Error('PORT not set');
+}
+
+if (!ENV) {
+  throw new Error('ENV not set');
+}
+
+if (!SESSION_SECRET) {
+  throw new Error('SESSION_SECRET not set');
+}
 
 const app = express();
 
@@ -22,15 +34,20 @@ app.use(bodyParser.json());
 
 app.use(
   session({
-    store: new redis({ url: `${REDIS_URL}:${REDIS_HOST_PORT}`, logErrors: true }),
+    store: new redis({
+      url: `${REDIS_URL}:${REDIS_HOST_PORT}`,
+      logErrors: true
+    }),
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.serializeUser((user, done) => {
-  console.log('serializing');
   return done(null, {
     id: user.id,
     username: user.username
@@ -38,7 +55,6 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((user, done) => {
-  console.log('deserializing');
   new User({ id: user.id })
     .fetch()
     .then(user => {
@@ -55,42 +71,29 @@ passport.deserializeUser((user, done) => {
 
 passport.use(
   new LocalStrategy(function(username, password, done) {
-    return new User({ username: username })
+    return User.query(qb => {
+      qb.whereRaw(`LOWER(username) LIKE ?`, [username]);
+    })
       .fetch()
       .then(user => {
-        user = user.toJSON();
         if (user === null) {
-          return done(null, false, { message: 'bad username or password' });
+          return done(null, false);
         } else {
+          user = user.toJSON({ visibility: false });
           bcrypt.compare(password, user.password).then(res => {
             if (res) {
               return done(null, user);
             } else {
-              return done(null, false, {
-                message: 'bad username or password'
-              });
+              return done(null, false);
             }
           });
         }
       })
       .catch(err => {
-        return done(err)
+        return done(err);
       });
   })
 );
-
-if (!PORT) {
-  throw new Error('PORT not set');
-}
-
-if (!ENV) {
-  throw new Error('ENV not set');
-}
-
-if (!SESSION_SECRET) {
-  throw new Error('SESSION_SECRET not set');
-}
-
 
 app.use('/api', auth);
 
