c196b2c42e2943563a897fe6b15adc421ceeba0c
kevin tauzer <kevint@kevins-MBP.home>
2019-03-01 12:11:26 -1000

    make foreign keys not nullable

--- database/migrations/20190301113754_nonnullable.js
+++ database/migrations/20190301113754_nonnullable.js
@@ -1,18 +1,18 @@
 
 exports.up = function(knex, Promise) {
  return knex.schema.alterTable('items', (table) => {
-  table.integer('created_by').references('id').inTable('users').notNullable().alter();
-  table.integer('status_id').references('id').inTable('statuses').notNullable().alter();
-  table.integer('category_id').references('id').inTable('categories').notNullable().alter();
-  table.integer('condition_id').references('id').inTable('conditions').notNullable().alter();
+  table.integer('created_by').notNullable().alter();
+  table.integer('status_id').notNullable().alter();
+  table.integer('category_id').notNullable().alter();
+  table.integer('condition_id').notNullable().alter();
  }) 
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.alterTable('items', (table) => {
-    table.integer('created_by').references('id').inTable('users').nullable().alter();
-    table.integer('status_id').references('id').inTable('statuses').nullable().alter();
-    table.integer('category_id').references('id').inTable('categories').nullable().alter();
-    table.integer('condition_id').references('id').inTable('conditions').nullable().alter();
+    table.integer('created_by').nullable().alter();
+    table.integer('status_id').nullable().alter();
+    table.integer('category_id').nullable().alter();
+    table.integer('condition_id').nullable().alter();
    })  
 };
