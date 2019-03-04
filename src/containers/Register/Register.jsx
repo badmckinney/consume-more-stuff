fc63c9d7d5ad28236068eb3753a7c8b048f5d4ef
kevin tauzer <kevint@kevins-MBP.home>
2019-03-04 11:30:11 -1000

    add styling to register page

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './Register.scss';
 
 class Register extends Component {
   constructor(props) {
@@ -64,62 +66,71 @@ class Register extends Component {
     }
 
     return (
-      <div>
-        <form>
-          <div>
-            <label htmlFor="first_name">First Name:</label>
-          </div>
-          <input
-            type="text"
-            name="first_name"
-            value={this.state.first_name}
-            onChange={this.handleInputOnChange}
-          />
+      <div className="register-wrapper">
+        <div className="register-container">
+          <form>
+            <div>
+              <label htmlFor="first_name">First Name:</label>
+            </div>
+            <input
+              type="text"
+              name="first_name"
+              value={this.state.first_name}
+              onChange={this.handleInputOnChange}
+            />
+            <div>
+              <label htmlFor="last_name">Last Name:</label>
+            </div>
+            <input
+              type="text"
+              name="last_name"
+              value={this.state.last_name}
+              onChange={this.handleInputOnChange}
+            />
 
-          <div>
-            <label htmlFor="last_name">Last Name:</label>
-          </div>
-          <input
-            type="text"
-            name="last_name"
-            value={this.state.last_name}
-            onChange={this.handleInputOnChange}
-          />
+            <div>
+              <label htmlFor="email">Email:</label>
+            </div>
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleInputOnChange}
+            />
 
-          <div>
-            <label htmlFor="email">Email:</label>
-          </div>
-          <input
-            type="text"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputOnChange}
-          />
+            <div>
+              <label htmlFor="username">Username:</label>
+            </div>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputOnChange}
+            />
 
-          <div>
-            <label htmlFor="username">Username:</label>
-          </div>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleInputOnChange}
-          />
+            <div>
+              <label htmlFor="password">Password:</label>
+            </div>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputOnChange}
+            />
 
-          <div>
-            <label htmlFor="password">Password:</label>
-          </div>
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleInputOnChange}
-          />
+            <div className="login-here">
+              {' '}
+              Already have an account?
+              <Link to={'/login'}> Login here</Link>
+            </div>
 
-          <div>
-            <button onClick={this.handleSubmit}>Register</button>
-          </div>
-        </form>
+            <div className="btn-container">
+              <button className="btn" onClick={this.handleSubmit}>
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
