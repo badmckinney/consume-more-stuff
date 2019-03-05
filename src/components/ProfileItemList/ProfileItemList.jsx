7a986ac5967dfd9b404688a4b38c5e8827e6c169
Gene83 <gene83@protonmail.com>
2019-03-05 11:05:58 -1000

    import scss for profile item list

--- src/components/ProfileItemList/ProfileItemList.jsx
+++ src/components/ProfileItemList/ProfileItemList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
+import './ProfileItemList.scss';
 
 const ProfileItemList = props => {
   const itemList = props.items.map(item => {
