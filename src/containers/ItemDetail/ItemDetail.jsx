47f63c0bb79159b48660a1ab41312772940e4329
Gene83 <gene83@protonmail.com>
2019-03-04 14:07:07 -1000

    update itemDetail, add error handling, refactor render function

--- src/containers/ItemDetail/ItemDetail.jsx
+++ src/containers/ItemDetail/ItemDetail.jsx
@@ -7,127 +7,93 @@ import { Link } from 'react-router-dom';
 class ItemDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      notFound: false
+    };
+
+    this.editButton = this.editButton.bind(this);
   }
 
   componentDidMount() {
     const item = this.props.match.params.id;
-    this.props.loadItem(item);
+    this.props.loadItem(item).then(data => {
+      if (!data) {
+        return this.setState({ notFound: true });
+      }
+    });
+  }
+
+  editButton() {
+    if (this.props.item.createdBy === this.props.currentUser) {
+      return (
+        <Link to={`/items/${this.props.item.id}/edit`}>
+          <button>Edit</button>
+        </Link>
+      );
+    }
+
+    return <></>;
   }
 
   render() {
-    const detail = this.props.detail;
-    if (detail) {
-      if (this.props.detail.item.createdBy === this.props.currentUser) {
-        return (
-          <div className="detail-container">
-            <div className="detail-header">
-              <div className="detail-name">
-                <h3> {detail.item.name} </h3>
-              </div>
+    const item = this.props.item;
 
-              <div className="detail-price">
-                <h3> ${detail.item.price} </h3>
-              </div>
-            </div>
+    if (this.state.notFound) {
+      return <div className="error">Item not found</div>;
+    }
 
-            <div className="detail-image">
-              <img alt={detail.item.name} src="" />
-            </div>
-
-            <div className="detail-wrapper">
-              <div className="detail">
-                <p>Manufacurer: {detail.item.manufacturer} </p>
-              </div>
-
-              <div className="detail">
-                <p>Model: {detail.item.model} </p>
-              </div>
-
-              <div className="detail">
-                <p> Dimensions </p>
-                <p>
-                  L: {detail.item.length} W:{detail.item.width} H:
-                  {detail.item.height}
-                </p>
-              </div>
-
-              <div className="detail">
-                <p> Additional information</p>
-                <p> {detail.notes} </p>
-              </div>
-
-              <div className="detail">
-                <p> Views: {detail.item.views} </p>
-              </div>
-            </div>
-
-            <div className="detail-description">
-              <div>{detail.item.description}</div>
-            </div>
-
-            <Link to={`/items/${this.state.id}/edit`}>
-              <button>Edit</button>
-            </Link>
-          </div>
-        );
-      }
-      return (
-        <div className="detail-container">
-          <div className="detail-header">
-            <div className="detail-name">
-              <h3> {detail.item.name} </h3>
-            </div>
-
-            <div className="detail-price">
-              <h3> ${detail.item.price} </h3>
-            </div>
+    return (
+      <div className="detail-container">
+        <div className="detail-header">
+          <div className="detail-name">
+            <h3> {item.name} </h3>
           </div>
 
-          <div className="detail-image">
-            <img alt={detail.item.name} src="" />
-          </div>
-
-          <div className="detail-wrapper">
-            <div className="detail">
-              <p>Manufacurer: {detail.item.manufacturer} </p>
-            </div>
-
-            <div className="detail">
-              <p>Model: {detail.item.model} </p>
-            </div>
-
-            <div className="detail">
-              <p> Dimensions </p>
-              <p>
-                L: {detail.item.length} W:{detail.item.width} H:
-                {detail.item.height}
-              </p>
-            </div>
-
-            <div className="detail">
-              <p> Additional information</p>
-              <p> {detail.item.notes} </p>
-            </div>
-
-            <div className="detail">
-              <p> Views: {detail.item.views} </p>
-            </div>
-          </div>
-
-          <div className="detail-description">
-            <div>{detail.item.description}</div>
+          <div className="detail-price">
+            <h3> ${item.price} </h3>
           </div>
         </div>
-      );
-    }
-    return <> </>;
+        <div className="detail-image">
+          <img alt={item.name} src="" />
+        </div>
+        <div className="detail-wrapper">
+          <div className="item">
+            <p>Manufacurer: {item.manufacturer} </p>
+          </div>
+
+          <div className="detail">
+            <p>Model: {item.model} </p>
+          </div>
+
+          <div className="detail">
+            <p> Dimensions </p>
+            <p>
+              L: {item.length} W:{item.width} H:
+              {item.height}
+            </p>
+          </div>
+
+          <div className="detail">
+            <p> Additional information</p>
+            <p> {item.notes} </p>
+          </div>
+
+          <div className="detail">
+            <p> Views: {item.views} </p>
+          </div>
+        </div>
+        <div className="detail-description">
+          <div>{item.description}</div>
+        </div>
+        {this.editButton()}
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    detail: state.detail,
+    item: state.item,
     currentUser: state.currentUser
   };
 };
@@ -135,7 +101,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     loadItem: item => {
-      dispatch(loadSingleItem(item));
+      return dispatch(loadSingleItem(item));
     }
   };
 };
