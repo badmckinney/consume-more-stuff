37fb93cac7a8fd63fe964372542a4e3ca67d9ecd
kevin tauzer <kevint@kevins-MBP.home>
2019-03-05 20:00:04 -1000

    create html for css

--- src/containers/NewItem/NewItem.jsx
+++ src/containers/NewItem/NewItem.jsx
@@ -29,7 +29,7 @@ class NewItem extends Component {
 
   error() {
     if (this.state.isError) {
-      return <div className="error">error creating post</div>;
+      return <div className="error">Error creating post</div>;
     }
 
     return <></>;
@@ -89,147 +89,139 @@ class NewItem extends Component {
     }
 
     return (
-      <div className="add-item-page">
+      <div className="add-item-container">
         {this.error()}
-        <h2>Create Post:</h2>
+        <h2>Create New Posting</h2>
         <form>
-          <div>
-            <label htmlFor="category_id">Category:</label>
-          </div>
-          <select
-            name="category_id"
-            value={this.state.category_id}
-            onChange={this.handleInputOnChange}
-          >
-            <option value="1">Automotive</option>
-            <option value="2">Furniture</option>
-            <option value="3">Appliances</option>
-            <option value="4">Electronics</option>
-            <option value="5">Sporting Goods</option>
-            <option value="6">Jewelry</option>
-            <option value="7">Apparel</option>
-            <option value="8">Musical Instruments</option>
-            <option value="9">Wanted</option>
-          </select>
-
-          <div className="title-price">
-            <div>
-              <label htmlFor="title">Posting Title:</label>
+          <div className="top-box">
+            <div className="title-price-container">
+              <div className="title">
+                <div>Posting Title:</div>
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleInputOnChange}
+                />
+              </div>
+              <div className="price">
+                <div>Price</div>
+                <input
+                  type="text"
+                  name="price"
+                  value={this.state.price}
+                  onChange={this.handleInputOnChange}
+                />
+              </div>
             </div>
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleInputOnChange}
-            />
-
-            <div>
-              <label htmlFor="price">Price</label>
+            <div className="image">
+              <div>Image</div>
+              <input
+                type="text"
+                name="image"
+                value={this.state.image}
+                onChange={this.handleInputOnChange}
+              />
             </div>
-            <input
-              type="text"
-              name="price"
-              value={this.state.price}
-              onChange={this.handleInputOnChange}
-            />
+            <div className="description-container">
+              <div className="description">
+                <div>Posting Body</div>
+                <textarea
+                  name="description"
+                  value={this.state.description}
+                  onChange={this.handleInputOnChange}
+                />
+              </div>
+            </div>
           </div>
-
-          <div>
-            <label htmlFor="image">Image:</label>
-          </div>
-          <input
-            type="text"
-            name="image"
-            value={this.state.image}
-            onChange={this.handleInputOnChange}
-          />
-
-          <div>
-            <label htmlFor="description">Posting Body</label>
-          </div>
-          <textarea
-            name="description"
-            value={this.state.description}
-            onChange={this.handleInputOnChange}
-          />
 
           <div className="posting-details">
-            <div>
-              <label htmlFor="manufacturer">Make / Manufacturer</label>
-            </div>
-            <input
-              type="text"
-              name="manufacturer"
-              value={this.state.manufacturer}
-              onChange={this.handleInputOnChange}
-            />
-
-            <div>
-              <label htmlFor="model">Model Name / Number</label>
-            </div>
-            <input
-              type="text"
-              name="model"
-              value={this.state.model}
-              onChange={this.handleInputOnChange}
-            />
-
-            <div>
-              <label htmlFor="dimensions">Size / Dimensions</label>
-            </div>
-            <div className="dimensions">
-              L:
+            <div className="detail-container1">
+              <div className="manufacturer">Make / Manufacturer</div>
               <input
                 type="text"
-                name="length"
-                placeholder="length"
-                value={this.state.length}
+                name="manufacturer"
+                value={this.state.manufacturer}
                 onChange={this.handleInputOnChange}
               />
-              W:
+              <div className="model-name">Model Name / Number</div>
               <input
                 type="text"
-                name="width"
-                placeholder="width"
-                value={this.state.width}
+                name="model"
+                value={this.state.model}
                 onChange={this.handleInputOnChange}
               />
-              H:
-              <input
-                type="text-area"
-                name="height"
-                placeholder="height"
-                value={this.state.height}
+              <div className="dimension-name">Size / Dimensions</div>
+              <div className="dimensions">
+                <input
+                  type="text"
+                  name="length"
+                  placeholder="length"
+                  value={this.state.length}
+                  onChange={this.handleInputOnChange}
+                />
+
+                <input
+                  type="text"
+                  name="width"
+                  placeholder="width"
+                  value={this.state.width}
+                  onChange={this.handleInputOnChange}
+                />
+                <input
+                  type="text-area"
+                  name="height"
+                  placeholder="height"
+                  value={this.state.height}
+                  onChange={this.handleInputOnChange}
+                />
+              </div>
+            </div>
+
+            <div className="select-container">
+              <div className="category">
+                <div>Category</div>
+                <select
+                  name="category_id"
+                  value={this.state.category_id}
+                  onChange={this.handleInputOnChange}
+                >
+                  <option value="1">Automotive</option>
+                  <option value="2">Furniture</option>
+                  <option value="3">Appliances</option>
+                  <option value="4">Electronics</option>
+                  <option value="5">Sporting Goods</option>
+                  <option value="6">Jewelry</option>
+                  <option value="7">Apparel</option>
+                  <option value="8">Musical Instruments</option>
+                  <option value="9">Wanted</option>
+                </select>
+              </div>
+              <div className="condition">
+                <div>Condition</div>
+                <select
+                  name="condition_id"
+                  value={this.state.condition_id}
+                  onChange={this.handleInputOnChange}
+                >
+                  <option value="1">Poor</option>
+                  <option value="2">Fair</option>
+                  <option value="3">Great</option>
+                  <option value="4">Excellent</option>
+                  <option value="5">New In Box</option>
+                </select>
+              </div>
+            </div>
+            <div className="notes-container">
+              <div className="notes">Notes</div>
+              <textarea
+                name="notes"
+                value={this.state.notes}
                 onChange={this.handleInputOnChange}
               />
             </div>
-
-            <div>
-              <label htmlFor="condition_id">Condition</label>
-            </div>
-            <select
-              name="condition_id"
-              value={this.state.condition_id}
-              onChange={this.handleInputOnChange}
-            >
-              <option value="1">Poor</option>
-              <option value="2">Fair</option>
-              <option value="3">Great</option>
-              <option value="4">Excellent</option>
-              <option value="5">New In Box</option>
-            </select>
-
-            <div>
-              <label htmlFor="notes">Notes:</label>
-            </div>
-            <textarea
-              name="notes"
-              value={this.state.notes}
-              onChange={this.handleInputOnChange}
-            />
           </div>
-
-          <button onClick={this.handleSubmit}>Create Post</button>
+          <button className="btn" onClick={this.handleSubmit}>Create Post</button>
         </form>
       </div>
     );
