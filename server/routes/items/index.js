80a750df4d140a2cdcf7a748ced7735cb8736d64
kevin tauzer <kevint@kevins-MBP.home>
2019-03-07 14:08:57 -1000

    add isauthenticated to backend routes

--- server/routes/items/index.js
+++ server/routes/items/index.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 const upload = multer({ dest: 'server/uploads/' });
+const isAuth = require('../isAuth');
+
 
 const bookshelf = require('../../../database/models/bookshelf');
 const Item = require('../../../database/models/Item');
@@ -40,7 +42,7 @@ router.get('/items', (req, res) => {
     });
 });
 
-router.get('/items/owned', (req, res) => {
+router.get('/items/owned',isAuth, (req, res) => {
   const id = req.user.id;
 
   Item.where('created_by', id)
@@ -251,7 +253,7 @@ router.get('/items/:id', (req, res) => {
  * POST
  ************************/
 
-router.post('/items/new', upload.single('image'), (req, res) => {
+router.post('/items/new', upload.single('image'),isAuth, (req, res) => {
   const user = req.user;
 
   if (req.file) {
@@ -353,7 +355,7 @@ router.post('/items/new', upload.single('image'), (req, res) => {
  * PUT
  ************************/
 
-router.put('/items/:id', (req, res) => {
+router.put('/items/:id', isAuth,(req, res) => {
   const item_id = req.params.id;
   const user_id = req.user.id;
   const editedItem = {
@@ -403,7 +405,7 @@ router.put('/items/:id', (req, res) => {
  *  DELETE
  ************************/
 
-router.delete('/items/:id', (req, res) => {
+router.delete('/items/:id',isAuth, (req, res) => {
   const id = req.params.id;
   const user_id = req.user.id;
 
