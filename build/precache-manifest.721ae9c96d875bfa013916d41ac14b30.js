01554b525ad289aba0239698098a2998408ad9be
Brad McKinney <brad.mckin@gmail.com>
2019-03-08 13:45:31 -1000

    run npm build script

---
+++ build/precache-manifest.721ae9c96d875bfa013916d41ac14b30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5c7cbc03f8be5133c903",
+    "url": "/static/js/main.5c7cbc03.chunk.js"
+  },
+  {
+    "revision": "8e55c0a7439439dfdbd0",
+    "url": "/static/js/2.8e55c0a7.chunk.js"
+  },
+  {
+    "revision": "5c7cbc03f8be5133c903",
+    "url": "/static/css/main.e7979878.chunk.css"
+  },
+  {
+    "revision": "cd5b18cd19179835951fbb3e804886df",
+    "url": "/index.html"
+  }
+];
